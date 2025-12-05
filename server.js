fed3ef604bb77fce9a2616d3ec0ec7855171515c
mansorbelfai <102861923+mansour12355@users.noreply.github.com>
2025-12-05 18:36:00 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 
 // Middleware
 app.use(cors({
